067d745c372d8feebdf7668703531b24c7bea640
Zoe Tankersley <zoetankersley@Zoes-MacBook-Pro.local>
2025-11-27 09:41:33 -0500

    Fix run.sh for Docker, add .env.example, update Dockerfile for port 8000

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 export FLASK_APP=src.app
-export FLASK_ENV=production
-python3 -u -m flask run --host=0.0.0.0 --port=${PORT:-8000}
+export FLASK_ENV=${FLASK_ENV:-production}
+python3 -u -m flask run --host=0.0.0.0 --port=${PORT:-8080}
 
